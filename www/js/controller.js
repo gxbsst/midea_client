f752da527f7838d008d0bd7226bfb79f0bce5e66
gxbsst <gxbsst@gmail.com>
2015-07-31 20:41:41 +0800

    first commit.

--- www/js/controller.js
+++ www/js/controller.js
@@ -2,22 +2,65 @@ angular.module('starter.controllers', ['ngSanitize'])
     .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
 
     })
-    .controller('ProjectCtrl', function ($scope, Project, $ionicLoading) {
+    .controller('ProjectCtrl', function ($scope, Project, $ionicLoading, $q, $timeout) {
+        reload();
 
-        $scope.reload = function () {
+        function reload() {
             $ionicLoading.show({
                 template: '正在载入...'
             });
-            Project.query(function (projects) {
-                $scope.projects = projects;
-                console.log(projects);
-            })
-                .$promise.then(function () {
-                    $ionicLoading.hide();
-                });
-        };
+            var page = 0,
+                loader = function () {
+                    page++;
+                    loadData(page).then(function(data){
+                        if (page === data.total_pages) {
+                            page = 0;
+                        }
+                        $timeout(loader, 2000);
+                    });
+                };
+            loader();
+        }
 
-        $scope.reload();
+        function loadData(page) {
+            var defer = $q.defer();
+            Project.get({page: page}, function (projects) {
+                $scope.projects = projects.data;
+                $scope.total_pages = projects.total_pages;
+                $ionicLoading.hide();
+                defer.resolve(projects);
+            });
+            return defer.promise;
+        }
+
+        //reload();
+        //
+        //function reload () {
+        //    $ionicLoading.show({
+        //        template: '正在载入...'
+        //    });
+        //    var page = 0;
+        //    var total_pages = 0;
+        //    $interval(function() {
+        //        page++
+        //        loadData(page);
+        //        total_pages = $scope.total_pages;
+        //
+        //        if(page === total_pages) {
+        //            page = 0;
+        //        }
+        //    }, 3000);
+        //}
+        //
+        //function loadData(page) {
+        //    Project.get({page: page}, function (projects) {
+        //        $scope.projects = projects.data;
+        //        $scope.total_pages = projects.total_pages;
+        //    })
+        //        .$promise.then(function () {
+        //            $ionicLoading.hide();
+        //        });
+        //}
     })
 
     .controller('HuihuoCtrl', function ($scope, $stateParams, Huihuo, Project, $ionicLoading) {
--- www/js/service.js
+++ www/js/service.js
@@ -1,10 +1,12 @@
+var host = "http://localhost:4001";
+
 angular.module('starter.services', ['ngResource'])
 
     .service('Project', function ($resource, Actions) {
-        return $resource('http://localhost:4001/projects/:id', {id: '@id'}, Actions);
+        return $resource(host + '/projects/:id', {id: '@id', page: "@page"}, Actions);
     })
     .service('Huihuo', function ($resource, Actions) {
-        return $resource('http://localhost:4001/projects/:projectId/huihuos', {projectId: '@projectId'}, Actions);
+        return $resource(host + '/projects/:projectId/huihuos', {projectId: '@projectId', page: "@page"}, Actions);
     })
 
 
